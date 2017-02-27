51d22f21814c2e8a5b6e31c16035ae7c74d64d46
ColdNightHour <jdixo001@ucr.edu>
2017-02-26 23:04:40 -0800

    Added more functionality

--- sentiment.py
+++ sentiment.py
@@ -2,14 +2,19 @@ import json
 from os.path import join, dirname
 from watson_developer_cloud import AlchemyLanguageV1
 
-alchemy_language = AlchemyLanguageV1(api_key='e6463e0ddf15729480d20e77deede23c77da12c4')
-
+alchemy_language_a = AlchemyLanguageV1(api_key='e6463e0ddf15729480d20e77deede23c77da12c4')
+alchemy_language_b = AlchemyLanguageV1(api_key='ffba820d5062423ba1ba98f504f0c8f9e728815c')
 url = 'https://gateway-a.watsonplatform.net/calls' \
      # '-watson-personality-insights/'
 def getSentiment(text):
-    sentiment = json.dumps(alchemy_language.sentiment(text=text))
+    sentiment = json.dumps(alchemy_language_a.sentiment(text=text))
     sentiment = json.loads(sentiment)
     if sentiment['docSentiment']['type'] == 'neutral':
-        return 0;
+        return str(0);
     else:
-        return sentiment['docSentiment']['score']
+        return str(sentiment['docSentiment']['score'])
+
+def getEmotion(text):
+    emotion = json.dumps(alchemy_language_b.emotion(text=text))
+    emotion = json.loads(emotion)
+    return str(emotion['docEmotions'])
