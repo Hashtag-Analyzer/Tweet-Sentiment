7a1df67fff2d2c9e7dce41b2cd0925521747145d
ColdNightHour <jdixo001@ucr.edu>
2017-05-01 15:44:13 -0700

    Update from using the Alchemy api to the Bluemix Natural Language Processing api

--- sentiment.py
+++ sentiment.py
@@ -1,17 +1,14 @@
 import json
-from os.path import join, dirname
-from watson_developer_cloud import AlchemyLanguageV1
+from watson_developer_cloud import NaturalLanguageUnderstandingV1
+import watson_developer_cloud.natural_language_understanding.features.v1 as features
 
-alchemy_language_a = AlchemyLanguageV1(api_key='e6463e0ddf15729480d20e77deede23c77da12c4')
-alchemy_language_b = AlchemyLanguageV1(api_key='ffba820d5062423ba1ba98f504f0c8f9e728815c')
-url = 'https://gateway-a.watsonplatform.net/calls' \
-     # '-watson-personality-insights/'
-def getSentiment(text):
-    sentiment = json.dumps(alchemy_language_a.sentiment(text=text))
-    sentiment = json.loads(sentiment)
-    return str(sentiment['docSentiment'])
-
-def getEmotion(text):
-    emotion = json.dumps(alchemy_language_b.emotion(text=text))
-    emotion = json.loads(emotion)
-    return str(emotion['docEmotions'])
+def getData(user, pw, doc):
+    natural_language_understanding = NaturalLanguageUnderstandingV1(
+        version='2017-02-27',
+        username='069bc0b6-bc9d-4faa-998d-b9be65e796d2',
+        password='HO3TWhePZB86')
+    url = 'https://gateway.watsonplatform.net/natural-language-understanding/api'
+    response = natural_language_understanding.analyze(
+        text=doc,
+        features=[features.Sentiment(), features.Emotion()])
+    return json.dumps(response)
