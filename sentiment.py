ecbb47d8f4ea018220e8ef3b66966f5598d4d35c
ColdNightHour <jdixo001@ucr.edu>
2017-05-01 15:45:03 -0700

    Update from using the Alchemy api to the Bluemix Natural Language Processing api

--- sentiment.py
+++ sentiment.py
@@ -5,8 +5,8 @@ import watson_developer_cloud.natural_language_understanding.features.v1 as feat
 def getData(user, pw, doc):
     natural_language_understanding = NaturalLanguageUnderstandingV1(
         version='2017-02-27',
-        username='069bc0b6-bc9d-4faa-998d-b9be65e796d2',
-        password='HO3TWhePZB86')
+        username=user,
+        password=pw)
     url = 'https://gateway.watsonplatform.net/natural-language-understanding/api'
     response = natural_language_understanding.analyze(
         text=doc,
